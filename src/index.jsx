8aac52c601178130af24795417fb3c5cf0d3f128
Alex Merced <alex@alexmerced.dev>
2022-05-15 08:19:03 -0400

    react 18

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 import {App} from "./components/App"
+import { createRoot } from "react-dom/client";
 
-ReactDom.render(<App />, document.querySelector("#root"));
+const root = createRoot(document.querySelector("#root"));
+root.render(<App/>)
